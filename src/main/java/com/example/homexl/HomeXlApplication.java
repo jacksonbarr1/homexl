ca5110ef76af42fac4baa821e8c041217463f1a1
Jackson Barr <jacksonbarr2021@gmail.com>
2023-02-22 01:04:08 -0500

    Initial Commit

---
+++ src/main/java/com/example/homexl/HomeXlApplication.java
@@ -0,0 +1,13 @@
+package com.example.homexl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeXlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeXlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/homexl/HomeXlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.homexl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeXlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
